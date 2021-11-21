3cb216a81a9c3b5c53a265e419826a909b2bdb3f
nthomson0317 <nicholasgraythomson@gmail.com>
2021-11-21 17:32:08 -0500

    validate

--- milestone-project.py
+++ milestone-project.py
@@ -34,7 +34,30 @@ result = input("Please enter a value: ")
 #Validate user input
 
 def user_choice():
-    choice = input("Please enter a number (0-10:) ")
+
+    #Variables
+
+    #Initial
+    choice = 'WRONG'
+    acceptable_range = range(0,10)
+    within_range = False
+
+    #Two conditions to check
+    #Digit or within_range == False
+    while choice.isdigit() == False or within_range==False:
+        choice = input("Please enter a number (0-10:) ")
+
+        #Digit check
+        if choice.isdigit() == False:
+            print('Sorry that is not a digit!')
+        
+        #Range check
+        if choice.isdigit() == True:
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print('Sorry, you are out of acceptable range (0-10)')
+                within_range = False
 
     return int(choice)
 
