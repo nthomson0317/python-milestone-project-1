c8c28343a0e019ac8892697faf868ce0e10351ff
nthomson0317 <nicholasgraythomson@gmail.com>
2021-11-21 17:20:50 -0500

    user input

--- milestone-project.py
+++ milestone-project.py
@@ -20,3 +20,22 @@ display(row1,row2,row3)
 #[' ', ' ', ' ']
 #[' ', 'x', ' ']
 #[' ', ' ', ' ']
+
+###### ACCEPTING USER INPUT
+
+
+input("Please enter a value: ")
+
+result = input("Please enter a value: ")
+
+## input function returns a string so you may need to convert it as so:
+## int(result)
+
+#Validate user input
+
+def user_choice():
+    choice = input("Please enter a number (0-10:) ")
+
+    return int(choice)
+
+user_choice()
