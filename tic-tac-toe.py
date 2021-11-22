9590d4c5ad8b02719dc2f74925d3e7c158906ab7
nthomson0317 <nicholasgraythomson@gmail.com>
2021-11-22 10:26:43 -0500

    assign players

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,5 @@
+import os
+
 # 2 players should be able to play the game (both sitting at the computer)
 
 # The board should be printed out every time a player makes a move
@@ -6,6 +8,7 @@
 
 # Step 1 write a function that can print out a board. set up your board as a list, where each index 1-9 corresponds with a number on a number pad, so you get a 3 by 3 board representation
 def display_board(board):
+    #clear_output()
     print(board[1]+'|'+board[2]+'|'+board[3])
     print('------')
     print(board[4]+'|'+board[5]+'|'+board[6])
@@ -17,6 +20,25 @@ test_board = ['#','X','O','X','O','X','O','X','O','X']
 display_board(test_board)
 
 # Step 2: Write a function that can take in a player input and assign their marker as 'X' or 'O'. Think about using while loops to continually ask until you get a correct answer.
+
+def x_or_o():
+    marker = 'unassigned'
+
+    while marker != 'X' and marker != 'O':
+        marker = input('Player 1, choose X or O: ')
+        player1 = marker
+        
+        if player1 == 'X':
+            player2 = 'O'
+        else:
+            player2 = 'X'
+        print(player1,player2)
+        return (player1, player2)
+    
+x_or_o()
+
+
+    
 
 # Step 3: Write a function that takes in the board list object, a marker ('X' or 'O'), and a desired position (number 1-9) and assigns it to the board
 
