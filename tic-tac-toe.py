43429319a687330029a552d9808cd932839f5741
nthomson0317 <nicholasgraythomson@gmail.com>
2021-12-05 16:03:43 -0500

    fixed board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,11 +10,17 @@ import random
 # Step 1 write a function that can print out a board. set up your board as a list, where each index 1-9 corresponds with a number on a number pad, so you get a 3 by 3 board representation
 def display_board(board):
     #clear_output()
-    print(board[1]+'|'+board[2]+'|'+board[3])
-    print('------')
-    print(board[4]+'|'+board[5]+'|'+board[6])
-    print('------')
-    print(board[7]+'|'+board[8]+'|'+board[9])
+    print('   |   |')
+    print(' ' + board[1]+' | '+board[2]+' | '+board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4]+' | '+board[5]+' | '+board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7]+' | '+board[8]+' | '+board[9])
+    print('   |   |')
 
 test_board = ['#','X','O','X','O','X','O','X','O','X']
 test_board_2 = ['#',' ','O','X','O','X','O','X','O','X']
